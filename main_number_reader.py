95129aaeaa88b767c9788b1cf7d75bfdf89e9243
Igor  Gusev <84721241+iggsev@users.noreply.github.com>
2021-07-13 21:38:16 -0300

    Add files via upload

--- main_number_reader.py
+++ main_number_reader.py
@@ -9,35 +9,6 @@ from mlxtend.data import loadlocal_mnist
 from mlxtend.data import mnist_data
 
 from run_number_reader import *
-######################## INPUTS ###########################################
-
-
-# # NEURAL NETWORK PARAMETERS 
-# train_sizes=[784, 64,32, 10] # vector representing number of nodes per layer
-# epochs=1000
-# learningrate= 0.0005
-# seed=1
-
-
-# # load database
-# x,y  = loadlocal_mnist(
-#         images_path='/Users/PC/Desktop/TCC-final/leitordenumeros/train-images.idx3-ubyte', 
-#         labels_path='/Users/PC/Desktop/TCC-final/leitordenumeros/train-labels.idx1-ubyte')
-
-# # SET PARAMETERS 
-
-# # training size
-# train_size=5000
-
-# # ACTIVATION FUNCTION 
-# # hyperbolic tangent: tanh 
-# # sigmoid: sig
-# # sine: sin  
-
-# phi="sig"
-
-# # graphic options
-# N_runs= 1 # number of runs for histogram 
 
 ############################# DATABASE ADJUST ############################
 
